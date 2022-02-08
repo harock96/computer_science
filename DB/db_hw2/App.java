4efcb37582c8d83d7045991fcf9f81e2c070915f
HaRokJang <61371150+harock96@users.noreply.github.com>
2022-02-09 01:38:38 +0900

    Update App.java

--- DB/db_hw2/App.java
+++ DB/db_hw2/App.java
@@ -1,64 +1,68 @@
-package Hongik.ce.dbms;
+package hongik.ce.dbms;
 
-import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.sql.*;
 
-public class App
-{
-	public static void main(String[] args) throws SQLException
-	{
-	    try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/MariaDB_Connection_Practice", "root", "5465")) // create connection for a server installed in localhost, with a user "root"
-	    {
-	        try (Statement stmt = conn.createStatement()) // create a Statement
-	        {
-	        	String query1 = "SELECT S.rating, AVG(S.age) AS average_age "
-						+ "FROM Sailors S "
-						+ "WHERE S.age >= 18 "
-						+ "GROUP BY S.rating "
-						+ "HAVING COUNT(*) >= 2";
-	        	String query2 = "UPDATE Sailors SET age = 30.0 "
-						+ "WHERE sname = 'Andy'";
+public class App {
+	
+	public static void main(String[] args) throws SQLException {
 
-	        	// execute query
-	        	HashMap<Integer, Float> map_old = new HashMap();
-	        	System.out.println("========== Step1 ==========");
-	            try (ResultSet rs_old = stmt.executeQuery(query1))
-	            {
-	            	System.out.println("rating\t| average_age");
-	            	System.out.println("--------+------------");
-	                while (rs_old.next())
-	                {
-		                System.out.print(rs_old.getInt(1));
-		                System.out.print("\t| ");
-		                System.out.println(rs_old.getFloat(2));
-		                map_old.put(rs_old.getInt(1), rs_old.getFloat(2));
-		            }
+	//create connection for a server installed in localhost, with a user "root" with no password
+	    try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/hw2", "root",'j02030339...')) { //비밀번호 부분은 변경
+	        // create a Statement
+	        try (Statement stmt = conn.createStatement()) {
+	        	
+	        	String step1 = "select S.rating, avg(S.age) as avgage "
+	        			+ "from Sailors S "
+	        			+ "where S.age >= 18 "
+	        			+ "group by S.rating "
+	        			+ "having count(*)>1";
+	        	
+	        	String step2 = "update sailors set age = 30 where sname = 'Andy'";
+	        	
+	        	HashMap<Integer,Float> map = new HashMap();
+	        	HashMap<Integer,Float> map2 = new HashMap();
+	  
+	            //execute query
+	            try (ResultSet rs = stmt.executeQuery(step1)) {
+	            	
+	            	System.out.println("rating\taveage");
+	                
+	                while(rs.next()) {
+	     
+		                System.out.print(rs.getInt(1)); 
+		                System.out.println("\t" + rs.getFloat(2));
+		                map.put(rs.getInt(1), rs.getFloat(2));
+		                }  
 	            }
-
-	            System.out.println("\n\n========== Step2 ==========");
-	            try (ResultSet dummy = stmt.executeQuery(query2))
-	            {
-	            	System.out.println("Successfully updated!");
+	            
+	            try (ResultSet rs2 = stmt.executeQuery(step2)){
 	            }
-	            HashMap<Integer, Float> map_new = new HashMap();
-	            try (ResultSet rs_new = stmt.executeQuery(query1))
-	            {
-	                while(rs_new.next())
-	                	map_new.put(rs_new.getInt(1), rs_new.getFloat(2));
+	            
+	            try (ResultSet rs3 = stmt.executeQuery(step1)) {
+	            	
+	            	
+	                while(rs3.next()) {
+	                	map2.put(rs3.getInt(1), rs3.getFloat(2));
+	                	
+	                	
+	                }
 	            }
-	            System.out.print("Diff. rating: ");
-	            for (Integer key : map_old.keySet())
-	            {
-	            	if ((float)map_old.get(key) != (float)map_new.get(key))
-	            	{
-	            		System.out.println(key);
-	            		break ;
-	            	}
-
+		                    
+	
+	            
+	            for (Integer key : map.keySet()) {
+	           
+	            	if ((double)map.get(key)!=(double)map2.get(key)) {
+	            	
+	            		System.out.println("Changed rating : " + key);
+	            		
+	            	}   	
 	            }
 	        }
 	    }
 	}
 }
+
