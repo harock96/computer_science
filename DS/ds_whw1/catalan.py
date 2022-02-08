785c3bfc19429017f4c369190ed39560f64f64a3
HaRokJang <61371150+harock96@users.noreply.github.com>
2022-02-09 02:02:16 +0900

    Update catalan.py

--- DS/ds_whw1/catalan.py
+++ DS/ds_whw1/catalan.py
@@ -1,27 +1,25 @@
-# A dynamic programming based function to find nth
-# Catalan number
+def memoize(f):
+    memo = {}
+    def helper(x):
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+    return helper
 
 def catalan(n):
+    res = 0
     if (n == 0 or n == 1):
         return 1
+    else:
+        for i in range(n):
+            res += catalan(i) * catalan(n-i-1)
+        return res
 
-    # Table to store results of subproblems
-    catalan =[0]*(n+1)
+catalan=memoize(catalan)
 
-    # Initialize first two values in table
-    catalan[0] = 1
-    catalan[1] = 1
+for i in range(100):
+    print(catalan(i), end = " ")
 
-    # Fill entries in catalan[]
-    # using recursive formula
-    for i in range(2, n + 1):
-        for j in range(i):
-            catalan[i] += catalan[j]* catalan[i-j-1]
 
-    # Return last entry
-    return catalan[n]
 
-# Driver code
-for i in range(101):
-	print(i, end=" :\t")
-	print(catalan(i), end="\n")
+
