e4d42e27f56a672ffd6fff6c89af34dbba0410a0
HaRokJang <61371150+harock96@users.noreply.github.com>
2022-02-09 01:58:33 +0900

    Update backend2.c

--- DS/ds_hw2/backend2.c
+++ DS/ds_hw2/backend2.c
@@ -1,77 +1,71 @@
 #include <stdio.h>
-#include <stdlib.h> // qsort
-#include <string.h> // strncmp
-#include "backend2.h"
+#include <stdlib.h>  // qsort
+#include <string.h>  // strncmp
+#include "backend2.h"     
 
 #define NUMBER_OF_RECORDS 20
 
+// record structure
 struct record {
   char name[3];
   char number[4];
 };
 
+// Declaration of the array for the main data
 struct record data[NUMBER_OF_RECORDS];
 
-int k = 0;
+// index of the first empty slot
+int k=0;  
 
-static void print_data(char *s, int n)
-{
-  int i;
-  for (i = 0; i < n; ++i)
-    putchar(s[i]);
-}
+// Declaration of functions visible only in this file.
+int cmp_record(const void *, const void *);
+int search_index(char [3]);
+void print_name(int);
+void print_number(int);
+void print_data(char *, int);
 
-static void print_name(int i)
-{
-  print_data(data[i].name, 3);
-}
-
-static void print_number(int i)
-{
-  print_data(data[i].number, 4);
-}
 
 void add(char *name, char *number)
 {
-  if (k < NUMBER_OF_RECORDS) {
-    (data[k].name)[0]=name[0];
-    (data[k].name)[1]=name[1];
-    (data[k].name)[2]=name[2];
-    (data[k].number)[0]=number[0];
-    (data[k].number)[1]=number[1];
-    (data[k].number)[2]=number[2];
-    (data[k].number)[3]=number[3];
+  if (k<NUMBER_OF_RECORDS) {
+    data[k].name[0]=name[0];
+    data[k].name[1]=name[1];
+    data[k].name[2]=name[2];
+    data[k].number[0]=number[0];
+    data[k].number[1]=number[1];
+    data[k].number[2]=number[2];
+    data[k].number[3]=number[3];
     print_name(k);
     printf(" : ");
     print_number(k);
     printf(" was successfully added!\n");
-    ++k;
+    k++;
   }
   else
     printf("Can't add.  Address book is full.\n");
 }
 
-static int search_index(char name[3])
-{
-  int i;
 
-  i = 0;
-  while (i < k) {
-    if ((data[i].name)[0] == name[0])
-      if ((data[i].name)[1] == name[1])
-        if ((data[i].name)[2] == name[2])
-          return (i);
-    ++i;
+// Returns the index of the found index.
+// Returns -1, if not found.
+int search_index(char *name)
+{
+  int i, j;
+  for (i = 0; i < k; i++) {
+    if (data[i].name[0] == name[0] && data[i].name[1] == name[1] && data[i].name[2] == name[2]) {
+      return i;
+    }
   }
-  return (-1);
+  return -1;
 }
 
-void search(char name[3])
+
+void search(char name[3])  
 {
   int s_result;
 
-  s_result = search_index(name);
-  if (s_result == -1)
+  s_result=search_index(name);
+  if (s_result==-1) 
     printf("Couldn't find the name.\n");
   else {
     print_name(s_result);
@@ -81,36 +75,56 @@ void search(char name[3])
   }
 }
 
-void delete(char name[3])
-{
-  int idx_to_del;
-  int i;
 
-  idx_to_del = search_index(name);
-  i = idx_to_del;
-  while (i < k - 1) {
-    (data[i].name)[0] = (data[i + 1].name)[0];
-    (data[i].name)[1] = (data[i + 1].name)[1];
-    (data[i].name)[2] = (data[i + 1].name)[2];
-    ++i;
+void delete (char name[3])
+{
+  // Use search_index()
+  int s_result;
+
+  s_result = search_index(name);
+  if (s_result == -1)
+    printf("Couldn't find the name.\n");
+  else {
+    for (s_result = s_result; s_result < k-1; s_result++) {
+      data[s_result].name[0] = data[s_result+1].name[0];
+      data[s_result].name[1] = data[s_result+1].name[1];
+      data[s_result].name[2] = data[s_result+1].name[2];
+      data[s_result].number[0] = data[s_result+1].number[0];
+      data[s_result].number[1] = data[s_result+1].number[1];
+      data[s_result].number[2] = data[s_result+1].number[2];
+      data[s_result].number[3] = data[s_result+1].number[3];
+    }
+    k--;
+    printf("The name was successfully deleted.\n");
   }
-  i = idx_to_del;
-  while (i < k - 1) {
-    (data[i].number)[0] = (data[i + 1].number)[0];
-    (data[i].number)[1] = (data[i + 1].number)[1];
-    (data[i].number)[2] = (data[i + 1].number)[2];
-    (data[i].number)[3] = (data[i + 1].number)[3];
-    ++i;
-  }
-  --k;
-  printf("The name was successfully deleted.\n");
 }
+
+
+void sort_list()
+{
+  
+
+  qsort(data, k, sizeof(struct record), cmp_record);
+/*   qsort(array, array_size, sizeof(struct record),  */
+/* 	(int (*)(const void*, const void*))cmp_record); */
+
+
+  // The message for command 'T'.
+  printf("Address Book is sorted now!\n");
+}
+
+int cmp_record(const void *r1, const void *r2)
+{
+  struct record * x = (struct record *) r1;
+  struct record * y = (struct record *) r2;
+  return strncmp(x->name, y->name, 3);
+}
+
 
 void print_list()
 {
   int i;
-  
-  for (i = 0; i < k; ++i) {
+  for (i=0; i<k; i++) {
     print_name(i);
     printf(" : ");
     print_number(i);
@@ -118,15 +132,22 @@ void print_list()
   }
 }
 
-static int cmp_record(const void *r1, const void *r2)
+// Prints ith name.
+void print_name(int i)
 {
-  struct record *x = (struct record *)r1;
-  struct record *y = (struct record *)r2;
-  return strncmp(x->name, y->name, 3);
+  print_data(data[i].name, 3);
 }
 
-void sort_list()
+// Prints ith number.
+void print_number(int i)
 {
-  qsort(data, k, sizeof(struct record), cmp_record);
-  printf("Address Book is sorted now!\n");
+  print_data(data[i].number, 4);
 }
+
+void print_data(char * s, int n)
+{
+  int i;
+  for (i=0; i<n; i++)
+    putchar(s[i]);
+}
+
